a11dd4308203e779d5cfa94716a8e4f30f99e3dc
kjirou <sorenariblog@gmail.com>
2019-10-22 10:56:01 +0900

    Mostly implemented

--- src/style-dumper.ts
+++ src/style-dumper.ts
@@ -1,3 +1,33 @@
-export const dumpStyles = () => {
-  return 'Hi'
+type StylesWithMetaData = {
+  children: StylesWithMetaData[],
+  className: string,
+  styles: {
+    [key: string]: string,
+  },
+  tagName: string,
+}
+
+// TODO: Add "depth" option
+// TODO: Add "selector(s)" option
+// TODO: Add "truncateLongValues" option
+export const dumpStyles = (element: HTMLElement): StylesWithMetaData => {
+  const styleDeclaration = window.getComputedStyle(element)
+  const styles: StylesWithMetaData['styles'] = {}
+  for (let i = 0; i < styleDeclaration.length; i++) {
+    const propertyName = styleDeclaration[i]
+    styles[propertyName] = styleDeclaration.getPropertyValue(propertyName)
+  }
+  const sortedStyles: StylesWithMetaData['styles'] = {}
+  Object.keys(styles).sort().forEach(key => {
+    sortedStyles[key] = styles[key]
+  })
+
+  const childElements = Array.prototype.slice.call(element.children)
+
+  return {
+    tagName: element.tagName,
+    className: element.getAttribute('class') || '',
+    styles: sortedStyles,
+    children: childElements.map(el => dumpStyles(el)),
+  }
 }
